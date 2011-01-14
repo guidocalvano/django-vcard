b434dc9388b9ccea48fe0220e13fa42882e64139
Guido Sales Calvano <guidocalvano@yahoo.com>
2011-01-14 15:23:17 +0100

    strange model bug

--- vcard/admin_views.py
+++ vcard/admin_views.py
@@ -4,7 +4,7 @@ from django.contrib.admin.views.decorators import staff_member_required
 import vobject
 from vobject.vcard import *
 from django.shortcuts import render_to_response
-import StringIO
+# import StringIO
 
 def select(request):
     return render_to_response(
@@ -23,12 +23,13 @@ def vcf_file_view(request,contact_set):
 
         vcf_file_content = vcf_file_content + c.convertTo( 'vCard' )
 
-    theFile = StringIO.StringIO()
+#     theFile = StringIO.StringIO()
 
     theFile.write( vcf_file_content ) 
 
     response = HttpResponse(FileWrapper(theFile), mimetype='application/force-download')
     response['Content-Disposition'] = 'attachment; filename=vcf_file.vcf'
+
     return response
 
 
