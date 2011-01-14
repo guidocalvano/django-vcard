28c439103fc3305871899221d61f4aec270acc0d
Guido Sales Calvano <guidocalvano@yahoo.com>
2011-01-15 00:16:59 +0100

    vcf to contact set

--- vcard/admin.py
+++ vcard/admin.py
@@ -122,7 +122,7 @@ class ContactAdmin(admin.ModelAdmin):
     def get_urls(self):
         urls = super(ContactAdmin, self).get_urls()
         my_urls = patterns('',
-            (r'^uploadVCF/$', self.admin_site.admin_view( self.uploadVCF ) ),
+            (r'^selectVCF/uploadVCF/$', self.admin_site.admin_view( self.uploadVCF ) ),
             (r'^selectVCF/$', self.admin_site.admin_view( self.selectVCF ) )
         )
         return my_urls + urls
