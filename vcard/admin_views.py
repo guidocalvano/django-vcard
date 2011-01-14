50a5b21d91b78409fc79bc778409ed9639169348
Guido Sales Calvano <guidocalvano@yahoo.com>
2011-01-14 14:54:20 +0100

    halfway making downloadable file

--- vcard/admin_views.py
+++ vcard/admin_views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render_to_response
 from django.contrib.admin.views.decorators import staff_member_required
 import vobject
 from vobject.vcard import *
+from django.shortcuts import render_to_response
+import StringIO
 
 def select(request):
     return render_to_response(
@@ -12,6 +14,22 @@ def select(request):
     )
 select = staff_member_required(select)
 
+
+def vcf_file_view(request,contact_set):
+
+    vcf_file_content = ""
+
+    for c in contact_set :
+
+        vcf_file_content = vcf_file_content + c.convertTo( 'vCard' )
+
+    theFile = StringIO.StringIO()
+
+    theFile.write( vcf_file_content ) 
+
+    response = HttpResponse(FileWrapper(theFile), mimetype='application/force-download')
+    response['Content-Disposition'] = 'attachment; filename=vcf_file.vcf'
+    return response
 
 
 
